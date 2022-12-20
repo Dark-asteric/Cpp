f2c86ff8a3073a9940e01be27e7ad75cf88e5da8
Muhammad MI Shafi <111156790+Dark-asteric@users.noreply.github.com>
2022-12-20 20:15:29 +0100

    Add files via upload

---
+++ Binary_to_Decimal.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+	//cout<<(1<<6);
+	string st;
+	cin>>st;
+	long long int sum = 0;
+	reverse(st.begin(),st.end());
+	for(int i=0; i<st.size(); i++){
+		if(st[i] == '1')
+			sum += (1<<i);
+	}
+	cout<<sum<<endl;
+	return 0;
+}
