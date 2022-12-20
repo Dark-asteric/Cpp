959f1a6cea00fd5087f014026c290df067c611f8
Muhammad MI Shafi <111156790+Dark-asteric@users.noreply.github.com>
2022-12-20 20:03:00 +0100

    Add files via upload

---
+++ Deci_to_Binary.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	int n,m; cin>>n;
+	m=n;
+	vector<int> v;
+	while(n>0){
+		int rem = n%2;
+		v.push_back(rem);
+		n /= 2;
+	}
+	int sz=v.size();
+	cout<<"\nDecimal to Binary :\t";
+	for(int i=sz-1;i>-1; i--)cout<<v[i];
+	cout<<endl;
+	//Simple way to convert Decimal to Binary : 
+	cout<<"\nDecimal to Binary :\t";
+	for(int i=10; i>=0; i--)
+		cout<< ((m>>i) & 1);
+	cout<<endl;
+}
