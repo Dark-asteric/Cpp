d226671f768bdb724e7f66d71f9e7604bc989c1e
Muhammad MI Shafi <111156790+Dark-asteric@users.noreply.github.com>
2022-12-20 22:50:39 +0100

    Add files via upload

---
+++ swap_two_numbers_with_bitwise.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	int a = 5 , b = 7;
+	cout<<"Before swap :\ta = "<<a<<" and b = "<<b<<endl;
+	a ^= b;
+	b ^= a;
+	a ^= b;
+	cout<<"After swap :\ta = "<<a<<" and b = "<<b;
+	return 0;
+}
