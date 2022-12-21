f1ffeb202f28c05ac49f23a6c5ff652fb5fca31e
Muhammad MI Shafi <111156790+Dark-asteric@users.noreply.github.com>
2022-12-21 14:35:25 +0100

    Add files via upload

---
+++ find_is_power_of_2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+bool ispowerof2(int n){
+	return (n && !(n & (n-1)));
+}
+int main(){
+	ios::sync_with_stdio(true);
+	cin.tie(0);
+	int n; cin>>n;
+	if(ispowerof2(n)) cout<<"True\n";
+	else cout<<"False\n";
+	return 0;
+}
